7a60c9cc64d3c5aaff4dbdff63215860764a8cb2
陈涛 <tao.chen@palmpay-inc.com>
2024-09-02 16:18:12 +0800

    feat: 新增

---
+++ 电子书/data.js
@@ -0,0 +1,12 @@
+const BOOK_TYPE = {
+  LEARN: "学习"
+}
+
+const bookArray = [
+  {
+    name: "TypeScript",
+    urlLink: "https://www.patrickzhong.com/TypeScript/",
+    bookType: BOOK_TYPE.LEARN,
+    bookTag: ["language", "前端"]
+  }
+]
